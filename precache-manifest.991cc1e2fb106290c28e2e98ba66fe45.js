63e93a4de8492ef8f7a508b9afb4cbf194dd666a
argun autalipov <argun63901@gmail.com>
2020-03-07 00:11:44 +0600

    Updates

---
+++ precache-manifest.991cc1e2fb106290c28e2e98ba66fe45.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e0bb582623a2d67ba09af12235fcad3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0727a63e04320dff4bbd",
+    "url": "/static/css/2.733ba2d0.chunk.css"
+  },
+  {
+    "revision": "acaf1553fc82b0ef1762",
+    "url": "/static/css/main.61678dcd.chunk.css"
+  },
+  {
+    "revision": "0727a63e04320dff4bbd",
+    "url": "/static/js/2.48beac0c.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.48beac0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acaf1553fc82b0ef1762",
+    "url": "/static/js/main.76792f17.chunk.js"
+  },
+  {
+    "revision": "30408383cb4feb40cad6",
+    "url": "/static/js/runtime-main.87b98ce7.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
