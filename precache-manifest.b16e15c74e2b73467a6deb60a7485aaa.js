05dd76e4101b1749ea67ea1e798af0ab2b6ad951
argun autalipov <argun63901@gmail.com>
2020-03-07 00:17:11 +0600

    Updates

---
+++ precache-manifest.b16e15c74e2b73467a6deb60a7485aaa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60ecf54eab7cfa955556005f3fecdbf9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "057a4fd2be6ddba319e0",
+    "url": "./static/css/2.733ba2d0.chunk.css"
+  },
+  {
+    "revision": "acaf1553fc82b0ef1762",
+    "url": "./static/css/main.61678dcd.chunk.css"
+  },
+  {
+    "revision": "057a4fd2be6ddba319e0",
+    "url": "./static/js/2.a5d52327.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "./static/js/2.a5d52327.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acaf1553fc82b0ef1762",
+    "url": "./static/js/main.76792f17.chunk.js"
+  },
+  {
+    "revision": "30dff8e42297a1b4209d",
+    "url": "./static/js/runtime-main.d59ec8c7.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.991cc1e2fb106290c28e2e98ba66fe45.js"
+  "./precache-manifest.b16e15c74e2b73467a6deb60a7485aaa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
