23ef264e8b1ee99a672b3d4251ca0ee2cca2ffa9
argun autalipov <argun63901@gmail.com>
2020-03-08 00:31:44 +0600

    asd

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import CourseSelector from './components/CourseSelector/CourseSelector';
 // Init VK  Mini App
 bridge.send('VKWebAppInit');
 
-let url = "https://y4m4t0.github.io/mentorium-vk-app"
+let url = "http://localhost:10888"
 
 // Если вы хотите, чтобы ваше веб-приложение работало в оффлайне и загружалось быстрее,
 // расскомментируйте строку с registerServiceWorker();
