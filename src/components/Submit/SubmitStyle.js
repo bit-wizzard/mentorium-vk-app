b8bd0e7d3a6723e64bf95d383135e39a39356dc5
y4m4t0 <argun63901@gmail.com>
2020-03-18 03:21:48 +0600

    hello

--- src/components/Submit/SubmitStyle.js
+++ src/components/Submit/SubmitStyle.js
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 export const SubmitStyle = styled.div 
 `
+    padding-bottom: 4rem;
     .test-header {
         background: #f0f0f0;
         padding: 1rem;
@@ -10,6 +11,34 @@ export const SubmitStyle = styled.div
         height: 20vh;  
         border-radius: 0; 
     }
-    .notification {
+`
+
+export const SubminContent = styled.div 
+`   
+    padding: 1rem;
+    .card {
+        border-radius: 10px;
+        padding: 1rem;
+        background: #fbfbfb;
     }
-    `
+    .question {
+        display: grid;
+        grid-gap: 0.5rem;
+        padding: 1rem;
+        background: #fff;
+        border-radius: 10px;
+    }
+    .answer {
+        padding: 1rem;
+        background: #fbfbfb;
+        margin: 0.5rem 0;
+        display: grid;
+        border-radius: 10px;
+    }
+    .correct {
+        padding: 1rem;
+        display: grid;
+        background: rgba(130, 222, 139, 0.5);
+        border-radius: 10px;
+    }
+`
